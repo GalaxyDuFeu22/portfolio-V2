d539c3a53a3b38e30a60e4fbf3ac1fbd5e62dbfa
GalaxyDuFeu22 <grouxoceane@gmail.com>
2026-07-01 12:37:33 +0200

    Ajout flèche pour remonter

--- JavaScript/animation.js
+++ JavaScript/animation.js
@@ -16,3 +16,31 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const scrollToTop = document.getElementById("top-arrow")
+
+window.addEventListener("scroll", function(){
+    if(window.pageYOffset>300) {
+        scrollToTop.style.opacity="0.5";
+        scrollToTop.style.transform="translateY(0)";
+    } else {
+        scrollToTop.style.opacity="0";
+        scrollToTop.style.transform="translateY(150%)";
+    }
+});
+
+scrollToTop.addEventListener("mouseover", function(){
+     scrollToTop.style.opacity="1";
+     scrollToTop.style.cursor="pointer";
+     
+});
+
+scrollToTop.addEventListener("mouseout", function(){
+    scrollToTop.style.opacity="0.5";
+});
+
+scrollToTop.addEventListener("click", function(){
+    window.scrollTo({
+        top:0
+    });
+});
